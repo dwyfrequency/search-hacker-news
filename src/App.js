790938a44c149b9b32733f5bd67f2a50841fcaf9
dwyfrequency <jdwy215@gmail.com>
2019-01-14 07:52:26 -0500

    adding logo and starter structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import Logo from "./assets/logo-hn-search.png";
 
 class App extends Component {
   render() {
-    return <div className="App">placeholder</div>;
+    return (
+      <div className="App">
+        <header>
+          <div className="TopHeader">
+            <img src={Logo} alt="logo" />
+            <form>
+              <input
+                type="text"
+                placeholder="Search Stories by title, url or author<"
+              />
+            </form>
+            <div className="Toolbar">ToolBar</div>
+          </div>
+          <div className="SearchFilters">
+            <button>Stories</button>
+            <button>Popularity</button>
+            <button>All time</button>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
 
