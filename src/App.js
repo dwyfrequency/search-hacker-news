c4ba8e216a170d159faf91828b71417c4fd59232
dwyfrequency <jdwy215@gmail.com>
2019-01-14 08:09:08 -0500

    adding html skeleton

--- src/App.js
+++ src/App.js
@@ -22,6 +22,58 @@ class App extends Component {
             <button>All time</button>
           </div>
         </header>
+        <main>
+          <div className="ArticleListItem">
+            <h2>
+              <a href="/">Article Title</a>
+            </h2>
+            <ul>
+              <li>
+                <a href="/">Points</a>
+              </li>
+              <li>
+                <a href="/">Author</a>
+              </li>
+              <li>
+                <a href="/">Age</a>
+              </li>
+              <li>
+                <a href="/">Comments</a>
+              </li>
+            </ul>
+          </div>
+          <div className="MoreResults">
+            <button>Page 1</button>
+          </div>
+        </main>
+        <footer>
+          <ul>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Settings</a>
+            </li>
+            <li>
+              <a href="">Help</a>
+            </li>
+            <li>
+              <a href="">API</a>
+            </li>
+            <li>
+              <a href="">Hacker News</a>
+            </li>
+            <li>
+              <a href="">Fork/Contribute</a>
+            </li>
+            <li>
+              <a href="">Status</a>
+            </li>
+            <li>
+              <a href="">Cool app</a>
+            </li>
+          </ul>
+        </footer>
       </div>
     );
   }
