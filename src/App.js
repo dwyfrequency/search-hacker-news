b18ac9ccd18654a8b3ba6983eb863348243477c3
dwyfrequency <jdwy215@gmail.com>
2019-01-16 07:18:40 -0500

    adding initial css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="Header">
           <div className="TopHeader">
-            <img src="" alt="logo" />
+            <img src={Logo} alt="logo" className="Logo" />
             <form>
               <input
                 type="text"
