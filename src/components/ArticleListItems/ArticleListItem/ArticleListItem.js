0807dfebf00f7200df8df015afadbd4eb9aa8ddf
dwyfrequency <jdwy215@gmail.com>
2019-01-20 13:48:44 -0500

    adding date function to display time

--- src/components/ArticleListItems/ArticleListItem/ArticleListItem.js
+++ src/components/ArticleListItems/ArticleListItem/ArticleListItem.js
@@ -1,8 +1,21 @@
 import React from "react";
 
+// const today =
+
 // age comes in as 2011-10-05T23:42:23.000Z
 const convertAge = isoString => {
-  const date = new Date(isoString);
+  const timeDifference = new Date().getTime() - new Date(isoString).getTime();
+  const day = 1000 * 60 * 60 * 24;
+
+  const days = Math.floor(timeDifference / day);
+  const months = Math.floor(days / 31);
+  const years = Math.floor(months / 12);
+  const dif = years
+    ? `${years} years ago`
+    : months
+    ? `${months} months ago`
+    : `${days} days ago`;
+  return dif;
 };
 
 const ArticleListItem = ({ author, points, title, url, ageIsoString }) => {
@@ -19,7 +32,7 @@ const ArticleListItem = ({ author, points, title, url, ageIsoString }) => {
           <a href="/">{author}</a>
         </li>
         <li>
-          <a href="/">Age</a>
+          <a href="/">{convertAge(ageIsoString)}</a>
         </li>
         <li>
           <a href="/">Comments</a>
